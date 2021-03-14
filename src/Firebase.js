181d6c2c5f896b9b0d17c45f18650eaaf788e3fd
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-03-14 11:15:35 +0530

    commit

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0q1nz3VGAQazZIIb3X3gifWYDBGmGjcA",
+    authDomain: "vaibhavcovid-19.firebaseapp.com",
+    projectId: "vaibhavcovid-19",
+    storageBucket: "vaibhavcovid-19.appspot.com",
+    messagingSenderId: "509151353588",
+    appId: "1:509151353588:web:2a2b1e6a42d1f5459e5ca1"
+};
